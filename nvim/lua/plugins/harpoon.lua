68e6872e1fe835c7c0e483b4ecc5f045d8d09215
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2025-03-06 10:12:50 +0100

    Harpoon append is deprecated, using add instead

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -7,7 +7,7 @@ return {
 
       harpoon:setup()
 
-      vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 
       vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
       vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
