7ee3cba1e4dd5dbe84dfd8ac01be9643d8719f72
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2025-03-10 11:16:35 +0100

    Add nvim-dap

--- .profile
+++ .profile
@@ -8,3 +8,4 @@ fi
 # User specific environment and startup programs
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$PATH:~/zig
+export PATH=$PATH:~/.local/share/nvim/mason/bin
---
+++ nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,68 @@
+return {
+  'mfussenegger/nvim-dap',
+  keys = { "<leader>r", "<F5>" },
+  dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
+  config = function()
+    local dap = require('dap')
+    local dap_utils = require('dap.utils')
+    local dapui = require('dapui')
+
+    dapui.setup({
+      mappints = {
+        edit = "i",
+        remove = "dd",
+      },
+    })
+
+    vim.keymap.set("n", "<F5>", dap.continue, {})
+    vim.keymap.set("n", "<F10>", dap.step_over, {})
+    vim.keymap.set("n", "<F11>", dap.step_into, {})
+    vim.keymap.set("n", "<F12>", dap.step_out, {})
+    vim.keymap.set("n", "<leader>r", dap.toggle_breakpoint, {})
+
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    local netcoredbg_path = vim.fn.exepath('netcoredbg')
+
+    dap.adapters.coreclr = {
+      type = 'executable',
+      command = netcoredbg_path,
+      args = {'--interpreter=vscode'}
+    }
+
+    dap.configurations.cs = {
+      {
+        type = "coreclr",
+        name = "Launch",
+        request = "launch",
+        program = function()
+          return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debut/', 'file')
+        end,
+      },
+      {
+        type = "coreclr",
+        name = "Attach",
+        request = "attach",
+        processId = function()
+          return dap_utils.pick_process({
+            filter = function(proc)
+              ---@diagnostic disable-next-line: return-type-mismatch
+              return proc.name:match(".*/Debug/.*") and not proc.name:find("vstest.console.dll")
+            end,
+          })
+        end,
+      },
+    }
+  end,
+}
