5cb93a2b464f71b38825e0876c3a426ed9e5293e
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2025-05-10 01:08:47 +0200

    Add script to install hyprland packages, update setup-config.sh

---
+++ hypr/hypr-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+cat <<"EOF"
+   ____         __       ____
+  /  _/__  ___ / /____ _/ / /__ ____
+ _/ // _ \(_-</ __/ _ `/ / / -_) __/
+/___/_//_/___/\__/\_,_/_/_/\__/_/
+
+EOF
+
+echo -e "Installing hyprland and dependencies for fedora"
+
+dnf install hyprland hyprpaper hyprlock hyprpolkitagent waybar swaync rofi-wayland ghostty
--- setup-config.sh
+++ setup-config.sh
@@ -31,6 +31,22 @@ if [ -d ~/.config/ghostty ]; then
   rm -rf ~/.config/ghostty
 fi
 
+if [ -d ~/.config/hypr ]; then
+  rm -rf ~/.config/hypr
+fi
+
+if [ -d ~/.config/rofi ]; then
+  rm -rf ~/.config/rofi
+fi
+
+if [ -d ~/.config/swaync ]; then
+  rm -rf ~/.config/swaync
+fi
+
+if [ -d ~/.config/waybar ]; then
+  rm -rf ~/.config/waybar
+fi
+
 if [ -d ~/.config/zls.json]; then
   rm -rf ~/.config/zls.json
 fi
@@ -38,4 +54,8 @@ fi
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/tmux ~/.config/tmux
 ln -s ~/.dotfiles/ghostty ~/.config/ghostty
+ln -s ~/.dotfiles/hypr ~/.config/hypr
+ln -s ~/.dotfiles/rofi ~/.config/rofi
+ln -s ~/.dotfiles/swaync ~/.config/swaync
+ln -s ~/.dotfiles/waybar ~/.config/waybar
 ln -s ~/.dotfiles/zls.json ~/.config/zls.json
