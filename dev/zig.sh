1971fda0463c0709cfd7b8eb86836c358aeaafd1
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2025-09-02 19:24:56 +0200

    Update

--- dev/zig.sh
+++ dev/zig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://ziglang.org/download/0.14.0/zig-linux-x86_64-0.14.0.tar.xz -q -O zig.tar.xz
+wget https://ziglang.org/download/0.15.1/zig-x86_64-linux-0.15.1.tar.xz -q -O zig.tar.xz
 
 rm -rf ~/zig
 tar -C ~ -xJf zig.tar.xz
