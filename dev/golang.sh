44700cc7c86977e7f34532c8d2523621a51244a5
Kristoffer Solvang-Jansen <krissjansen@gmail.com>
2025-09-28 14:52:58 +0200

    update

--- dev/golang.sh
+++ dev/golang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://go.dev/dl/go1.24.0.linux-amd64.tar.gz -q -O go.tar.gz
+wget https://go.dev/dl/go1.25.0.linux-amd64.tar.gz -q -O go.tar.gz
 
 rm -rf /usr/local/go
 tar -C /usr/local -xzf go.tar.gz
